092ce09b80ec6179e75e1db627df2d7d8ef51ce4
pere busquet <36750713+buski87@users.noreply.github.com>
2025-04-24 13:45:08 +0200

    fix error

--- api-rick-and-morty-app/components/Button.jsx
+++ api-rick-and-morty-app/components/Button.jsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 
 export default function Button({ children, onClick, href, className = '' }) {
-  const base = 'px-4 py-2 rounded bg-blue-600 text-white hover:bg-blue-700 transition text-center';
+  const base = 'px-4 py-2 rounded transition text-center';
 
   if (href) {
     return (
